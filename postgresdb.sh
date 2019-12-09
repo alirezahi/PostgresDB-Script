6af466be1d3c1f0ffef91ac48e8d5fac6ef090b6
Alireza Heydari <alireza97hi@gmail.com>
2019-12-09 08:30:57 +0330

    update db

--- postgresdb.sh
+++ postgresdb.sh
@@ -6,3 +6,7 @@ read -p 'Databse Name: ' db_name
 sudo -u postgres psql -c "CREATE DATABASE $db_name;"
 sudo -u postgres psql -c "CREATE USER $db_user_username WITH PASSWORD '$db_user_password';"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $db_name TO $db_user_username;"
+
+sudo -u postgres psql -c "CREATE DATABASE qadb;"
+sudo -u postgres psql -c "CREATE USER qadb_user WITH PASSWORD 'thi3ha3tobeap@ssword';"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE qadb TO qadb_user;"
